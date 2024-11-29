75acbb4c330dbb822fda772a367769f3d652acf0
Gregoire111278 <82652774+Gregoire111278@users.noreply.github.com>
2024-11-29 11:34:57 +0100

    Commit affichage et modif finales

--- frontend/src/app/components/create-project/create-project.component.ts
+++ frontend/src/app/components/create-project/create-project.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {ProjectService} from "../../services/projects.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-create-project',
@@ -10,19 +11,21 @@ import {ProjectService} from "../../services/projects.service";
 export class CreateProjectComponent {
   projectForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private projectService: ProjectService) {
+  constructor(private fb: FormBuilder, private projectService: ProjectService, private router: Router) {
     this.projectForm = this.fb.group({
       name: ['', Validators.required],
       description: [''],
-      ownerId: [''],
+      ownerId: [localStorage.getItem('id')],
     });
   }
 
 
   onsubmit() {
     if (this.projectForm.valid) {
+     this.router.navigate(['/projects']);
       this.projectService.createProject(this.projectForm.value).subscribe(response => {
         console.log(response);
+
       }, error => {
         console.error(error);
       });
